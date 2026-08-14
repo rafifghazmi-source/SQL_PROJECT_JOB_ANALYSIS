3fff571f9f7438b4f25f2eca7e0604f71c7a88a7
rafifghazmi <shidqirafif62@gmail.com>
2026-08-14 23:16:23 +0700

    Create and Modify Database and Table

--- sql_load/1_create_database.sql
+++ sql_load/1_create_database.sql
@@ -1,3 +1,6 @@
+-- Create a new database named 'sql_course'. This command initializes the database and prepares it for use.
 CREATE DATABASE sql_course;
 
--- MAKE SURE TO DROP CURRENT EXISTS DATABASE BEFORE CREATING A NEW ONE
+
+-- Delete the "--" if you want to drop the database. This command will remove the 'sql_course' database and all its contents permanently.
+-- DROP DATABASE sql_course;
